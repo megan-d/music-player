97c137cf36118a2ee33e4ae1a8ef1e385ec62fc8
Megan D <45672063+megan-d@users.noreply.github.com>
2022-12-29 13:06:21 -0700

    Complete basic music player and functions

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
+from tkinter import filedialog
 from tkinter import *
 import pygame
 import os
+from player_actions import (
+    load_music,
+    root,
+    all_songs,
+    current_song,
+    is_paused,
+    play_music,
+    pause_music,
+    next,
+    previous,
+)
 
-player = Tk()
 
 # Configure appearance of GUI
-player.title("Python Music Player")
-player.geometry("800x600")
+root.title("Python Music Player")
+root.geometry("800x600")
 
+# Create menu
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+organizer_menu = Menu(menu_bar, tearoff=False)
+organizer_menu.add_command(label="Select a Folder", command=load_music)
+menu_bar.add_cascade(label="Select Music", menu=organizer_menu)
 
 # Initialize pygame mixer module for loading and playing sounds
 pygame.mixer.init()
 
 
+# Populate buttons in GUI
+
+play_btn_png = PhotoImage(file="play-button.png")
+pause_btn_png = PhotoImage(file="pause-button.png")
+next_btn_png = PhotoImage(file="play-next.png")
+prev_btn_png = PhotoImage(file="play-previous.png")
+
+controls_frame = Frame(root)
+controls_frame.pack()
+
+play_btn = Button(controls_frame, image=play_btn_png, borderwidth=0, command=play_music)
+pause_btn = Button(
+    controls_frame, image=pause_btn_png, borderwidth=0, command=pause_music
+)
+next_btn = Button(controls_frame, image=next_btn_png, borderwidth=0, command=next)
+prev_btn = Button(controls_frame, image=prev_btn_png, borderwidth=0, command=previous)
+
+prev_btn.grid(row=0, column=0, padx=10, pady=10)
+play_btn.grid(row=0, column=1, padx=10, pady=10)
+pause_btn.grid(row=0, column=2, padx=10, pady=10)
+next_btn.grid(row=0, column=3, padx=10, pady=10)
+
 # Put everything on the display and respond to user input until the program terminates
-player.mainloop()
+root.mainloop()
---
+++ player_actions.py
@@ -0,0 +1,67 @@
+import os
+from tkinter import filedialog
+from tkinter import *
+import pygame
+
+root = Tk()
+
+# Create listbox
+songs_list = Listbox(root, bg="black", fg="white", width=100, height=30)
+songs_list.pack()
+
+all_songs = []
+current_song = None
+is_paused = False
+
+
+def load_music():
+    global all_songs, current_song
+    root.directory = filedialog.askdirectory()
+    for song in os.listdir(root.directory):
+        name, ext = os.path.splitext(song)
+        if ext == ".mp3":
+            all_songs.append(song)
+
+    for song in all_songs:
+        songs_list.insert("end", song)
+
+    songs_list.selection_set(0)
+    current_song = all_songs[songs_list.curselection()[0]]
+
+
+def play_music():
+    global current_song, is_paused
+    if not is_paused:
+        pygame.mixer.music.load(os.path.join(root.directory, current_song))
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.unpause()
+        is_paused = False
+
+
+def pause_music():
+    global is_paused
+    pygame.mixer.music.pause()
+    is_paused = True
+
+
+def next():
+    global current_song, is_paused
+    try:
+        songs_list.selection_clear(0, END)
+        songs_list.selection_set(all_songs.index(current_song) + 1)
+        current_song = all_songs[songs_list.curselection()[0]]
+        play_music()
+    except:
+        pass
+
+
+def previous():
+    global current_song, is_paused
+    try:
+        songs_list.selection_clear(0, END)
+        songs_list.selection_set(all_songs.index(current_song) - 1)
+        current_song = all_songs[songs_list.curselection()[0]]
+        play_music()
+    except:
+        pass
